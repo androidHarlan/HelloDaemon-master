faab924484627ce886fe25481d0e8b5fa248ebfa
WeDox <1986821027@qq.com>
2018-04-25 09:56:54 +0800

    服务server

---
+++ sample/src/main/java/com/xdandroid/sample/App.java
@@ -0,0 +1,17 @@
+package com.xdandroid.sample;
+
+import android.app.*;
+
+import com.xdandroid.hellodaemon.*;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //需要在 Application 的 onCreate() 中调用一次 DaemonEnv.initialize()
+        DaemonEnv.initialize(this, TraceServiceImpl.class, DaemonEnv.DEFAULT_WAKE_UP_INTERVAL);
+        TraceServiceImpl.sShouldStopService = false;
+        DaemonEnv.startServiceMayBind(TraceServiceImpl.class);
+    }
+}
